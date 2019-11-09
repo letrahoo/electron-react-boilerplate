d9445ee71dd96a793083491412c633d3151e14bd
Letra312 <hxq15827562797@gmail.com>
2019-11-09 22:08:41 +0800

    :building_construction: chore: add eslint, stylelint, babel

--- app/main.dev.js
+++ app/main.dev.js
@@ -1,13 +1,13 @@
-const { app, BrowserWindow } = require('electron');
-// import { app, BrowserWindow } from 'electron';
+// const { app, BrowserWindow } = require('electron');
+import { app, BrowserWindow } from 'electron';
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win;
+let mainWindow = null;
 
 function createWindow() {
   // Create the browser window
-  win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -16,17 +16,17 @@ function createWindow() {
   });
 
   // and load the index.html of the app
-  win.loadFile('index.html');
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   // Open the DevTools
-  win.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed
-  win.on('closed', () => {
+  mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null;
+    mainWindow = null;
   });
 }
 
@@ -47,7 +47,7 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (win === null) {
+  if (mainWindow === null) {
     createWindow()
   }
 })
