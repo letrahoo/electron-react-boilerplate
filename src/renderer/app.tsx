d4f65cf7054c04b1867907464c5fbb1b853377b8
Letra312 <hxq15827562797@gmail.com>
2019-11-13 00:39:43 +0800

    :bug: fix: active redux devtools

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { DECREMENT_ACTION, INCREMENT_ACTION } from './actions';
 import Counter from './Components/Counter';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 import './app.scss';
 export class App {
   public async setup(): Promise<void | Element | React.Component> {
-    const rendered = () => render(
-      <Counter
-        value={store.getState()}
-        onIncrement={() => store.dispatch(INCREMENT_ACTION)}
-        onDecrement={() => store.dispatch(DECREMENT_ACTION)}
-      />,
-      document.getElementById('app'),
-    );
+    const rendered = () =>
+      render(
+        <Counter
+          value={store.getState()}
+          onIncrement={() => store.dispatch(INCREMENT_ACTION)}
+          onDecrement={() => store.dispatch(DECREMENT_ACTION)}
+        />,
+        document.getElementById('app'),
+      );
     const result = rendered();
     store.subscribe(rendered);
     return result;
